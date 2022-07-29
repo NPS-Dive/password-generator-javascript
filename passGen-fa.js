bb1108707d5233b28b0d76d238b407443d7398dd
NPS-Dive <101453322+NPS-Dive@users.noreply.github.com>
2022-07-30 00:01:54 +0430

    Add files via upload

---
+++ passGen-fa.js
@@ -0,0 +1,40 @@
+let passLen = document.getElementById("pass-len-selector").value;
+const generatePass = document.getElementById("btn");
+const copyBTN = document.getElementById("copy");
+let passLenINT = parseInt(passLen);
+const resetButton = document.getElementById("reset");
+
+const newPassword = (length) => {
+	let result = '';
+	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-+/_()*&^%$#@!!~';
+	let passwordLength = characters.length;
+	
+	for (let pass = 0; pass < length; pass++) {
+		result += characters.charAt(Math.floor(Math.random() * passwordLength));
+	}
+	return result;
+}
+
+function displayPassword(){
+	document.getElementById("result-pass").innerHTML = newPassword(passLenINT);
+}
+
+function copyToClipboard(){
+	let copyContent = document.getElementById("result-pass");
+	navigator.clipboard.writeText(copyContent.innerText).then(message => {
+		alert("پسورد تولیدی شما کپی شد: " + copyContent.innerText);
+	});
+}
+
+generatePass.addEventListener('click', function(){
+	displayPassword();
+});
+
+copyBTN.addEventListener('click', function(){
+	copyToClipboard();
+});
+
+resetButton.addEventListener('click', function (){
+	document.getElementById("pass-len-selector").value = null;
+	document.getElementById("result-pass").innerHTML = null;
+});
